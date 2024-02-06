c6b35e89e8a7c1f9beeac4e0d4271c284cc1d1b9
apyd <arturpydych@gmail.com>
2024-02-06 18:56:50 +0100

    tests: use number of assertions instead of expect.assertions()

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -3,39 +3,39 @@ import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    expect.hasAssertions();
+    expect.assertions(1);
     expect(simpleCalculator({ a: 1, b: 2, action: Action.Add })).toBe(3);
   });
 
   test('should subtract two numbers', () => {
-    expect.hasAssertions();
+    expect.assertions(1);
     expect(simpleCalculator({ a: 2, b: 1, action: Action.Subtract })).toBe(1);
   });
 
   test('should multiply two numbers', () => {
-    expect.hasAssertions();
+    expect.assertions(1);
     expect(simpleCalculator({ a: 4, b: 3, action: Action.Multiply })).toBe(12);
   });
 
   test('should divide two numbers', () => {
-    expect.hasAssertions();
+    expect.assertions(1);
     expect(simpleCalculator({ a: 10, b: 2, action: Action.Divide })).toBe(5);
   });
 
   test('should exponentiate two numbers', () => {
-    expect.hasAssertions();
+    expect.assertions(1);
     expect(simpleCalculator({ a: 2, b: 3, action: Action.Exponentiate })).toBe(
       8,
     );
   });
 
   test('should return null for invalid action', () => {
-    expect.hasAssertions();
+    expect.assertions(1);
     expect(simpleCalculator({ a: 1, b: 2, action: undefined })).toBe(null);
   });
 
   test('should return null for invalid arguments', () => {
-    expect.hasAssertions();
+    expect.assertions(1);
     expect(simpleCalculator({ a: '2', b: '4', action: Action.Add })).toBe(null);
   });
 });
