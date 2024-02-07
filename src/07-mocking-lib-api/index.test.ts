1daa7eda5c32db12625175da3526932148d2cd37
apyd <arturpydych@gmail.com>
2024-02-07 17:52:14 +0100

    tests: add unit tests for 07-mocking-lib-api module

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,62 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
+
+jest.mock('axios');
 
 describe('throttledGetDataFromApi', () => {
+  const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
+  beforeEach(() => {
+    mockedAxios.create.mockReturnThis();
+    mockedAxios.get = jest.fn().mockResolvedValue({ data: '' });
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    expect.assertions(1);
+    const relativePath = '/posts/1';
+    await throttledGetDataFromApi(relativePath);
+    expect(mockedAxios.create).toHaveBeenCalledWith({
+      baseURL: 'https://jsonplaceholder.typicode.com',
+    });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    expect.assertions(1);
+    const relativePath = '/posts/1';
+    const promise = await throttledGetDataFromApi(relativePath);
+
+    jest.runAllTimers();
+
+    await promise;
+
+    expect(mockedAxios.get).toHaveBeenCalledWith(relativePath);
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    expect.assertions(1);
+
+    const relativePath = '/posts/1';
+    const mockData = { id: 1, title: 'first post' };
+
+    mockedAxios.get.mockResolvedValue({ data: mockData });
+
+    const promise = await throttledGetDataFromApi(relativePath);
+
+    jest.runAllTimers();
+
+    const data = await promise;
+    expect(data).toEqual(mockData);
   });
 });
